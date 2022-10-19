7f155a08cc365ba394f22a5c669b8c2bd9ee513e
Jecolia <jecoliatieleo@gmail.com>
2022-10-18 20:12:48 -0700

    file

---
+++ get_printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * get_printf - get_printf to get function from function.c
+ * @ap: arg
+ * @c: char
+ * Return: char
+ */
+int get_printf(const char c, va_list ap)
+{
+	int i = 0;
+	int j = 0;
+	print_t type[] = {
+		{'c', pchar},
+		{'s', pstr},
+		{'%', pperc},
+		{'d', pint},
+		{'i', pint},
+		{'b', pbin},
+		{'u', puns},
+		{'o', poct},
+		{'x', phex},
+		{'X', pHEX},
+		{'p', ppntr},
+};
+
+while (type[i].data != 0)
+{
+	if (type[i].data == c)
+		{
+		       j += type[i].f(ap);
+		       return (j);
+		}
+	       i++;
+}
+return (0);
+}
